0ac26dbd8da33b6bfece117ae54e07becc3d133b
sabuhi <sabuhi.alasgarli@pm.me>
2025-01-02 15:44:43 +0400

    feat: add some apps

---
+++ apps.sh
@@ -0,0 +1,4 @@
+# With PACMAN
+sudo pacman -S remmina freerdp virt-manager qemu-full putty kdiskmark qbittorrent discord keepassxc sqlitebrowser docker pycharm-community-edition gnome-tweaks torbrowser-launcher tor libreoffice-fresh syncthing
+# With YAY
+yay -S anydesk-bin visual-studio-code-bin zen-browser-bin
