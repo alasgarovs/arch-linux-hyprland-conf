31b27db92704bfb1db09ff036ee6f4871bf9e4d1
sabuhi <sabuhi.alasgarli@pm.me>
2026-01-21 00:29:41 +0400

    feat: update

--- packages/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install packages with pacman
-sudo pacman -S --noconfirm - < packages/pacman-packages.txt
-xdg-user-dirs-update
-
-# Install packages with yay
-yay -S --noconfirm - < packages/yay-packages.txt
-
